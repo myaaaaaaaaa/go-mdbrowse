00ff4e40bc18314740d167e3b3bc2dd7975692e2
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-10-04 18:44:39 -0400

    basic implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/myaaaaaaaaa/go-mdbrowse
+
+go 1.25.1
+
+require github.com/yuin/goldmark v1.7.13
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	_ "embed"
+
+	"github.com/yuin/goldmark"
+)
+
+func must[T any](val T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return val
+}
+
+//go:embed markdown.css
+var css string
+
+func mark2html(text string) string {
+	var buf bytes.Buffer
+
+	fmt.Fprintln(&buf, `<style>`)
+	fmt.Fprintln(&buf, css)
+	fmt.Fprintln(&buf, `</style>`)
+
+	err := goldmark.Convert([]byte(text), &buf)
+
+	if err != nil {
+		rawText := "markdown error: " + err.Error() + "\n\n" + text
+		return "<code>\n" + rawText + "\n</code>"
+	}
+	return buf.String()
+}
+
+func findMarkdownFiles() (rt []string) {
+	files := os.Args[1:]
+	if len(files) == 0 {
+		files = []string{"."}
+	}
+
+	for _, file := range files {
+		// For consistency
+		file = filepath.Clean(file)
+
+		err := filepath.WalkDir(file, func(p string, d os.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !d.IsDir() && strings.HasSuffix(p, ".md") {
+				rt = append(rt, p)
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return
+}
+
+func printLink(f string) {
+	f = must(filepath.Abs(f))
+	f = strings.ReplaceAll(f, " ", "%20")
+
+	fmt.Println("file://" + f)
+}
+
+func mktemp() string {
+	basedir := os.Getenv("XDG_RUNTIME_DIR")
+	if basedir == "" {
+		basedir = "/tmp"
+
+		user := os.Getenv("USER")
+		if user != "" {
+			basedir += "/" + user
+		}
+	}
+
+	basedir += "/markdown/"
+	basedir += fmt.Sprint(time.Now().Unix())
+	return basedir
+}
+
+func main() {
+	markdownFiles := findMarkdownFiles()
+
+	outDir := mktemp()
+	must(0, os.MkdirAll(outDir, 0700))
+
+	i := 0
+	for _, markdownFile := range markdownFiles {
+		i++
+		htmlFile := fmt.Sprintf("%s/%05d.htm", outDir, i)
+
+		data := must(os.ReadFile(markdownFile))
+		data = []byte(mark2html(string(data)))
+		must(0, os.WriteFile(htmlFile, data, 0600))
+
+		fmt.Print(markdownFile, "    ")
+		printLink(htmlFile)
+	}
+}
